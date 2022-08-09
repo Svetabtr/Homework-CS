b9ad679f77f46295eae4e7f627d840e08217a90a
Svetabtr <sveta-btr@mail.ru>
2022-08-09 15:21:55 +0300

    Программа длрешения задачи 2.1

---
+++ HW_2/Hometask_1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.Write("Введите трехзначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int FindSecondDigitNumber(int num)
+{
+    int SecondDigit = num/10%10;
+    return SecondDigit;
+}
+
+int result = FindSecondDigitNumber(Number);
+Console.WriteLine($"Вторая цифра этого числа - {result}");
