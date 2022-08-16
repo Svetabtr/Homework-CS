400f0caaae7b6f74efffe490d596d0642ce9c9ef
Svetabtr <sveta-btr@mail.ru>
2022-08-16 14:34:00 +0300

    Программа для решения задачи 4.1

---
+++ HW_4/Hometask_1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/ 
+
+Console.WriteLine("Введите два числа A и B: ");
+int numA = int.Parse(Console.ReadLine());
+int numB = int.Parse(Console.ReadLine()); 
+
+double NumberAPowToB (int num1, int num2)
+{
+    double result = Math.Pow(num1, num2);
+    return result;
+}
+
+double NewNum = NumberAPowToB(numA, numB);
+Console.WriteLine($"Число A в степени B = {NewNum}");
