ac079b571f5df9d48988468aa08687a4921d5581
Svetabtr <sveta-btr@mail.ru>
2022-08-16 14:45:35 +0300

    Программа для решения задачи 4.2

---
+++ HW_4/Hometask_2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Write("Введите любое число: ");
+int number = int.Parse(Console.ReadLine());
+
+int DigitsSum(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        int digit = num % 10;
+        result = result + digit;
+        num = num / 10;
+    }
+    return result;
+}
+
+int Summa = DigitsSum(number);
+Console.WriteLine($"Сумма чисел данного числа = {Summa}");
