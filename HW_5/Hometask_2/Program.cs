73096396b35768cda06983cee16cfb82d4658fe0
Svetabtr <sveta-btr@mail.ru>
2022-08-19 15:22:52 +0300

    Программа для решения задачи 36

---
+++ HW_5/Hometask_2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+стоящих на нечётных позициях(по индексу считаем).
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+Console.Clear();
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+int[] array = GetArray(10, 1, 100);
+
+int result = 0;
+for (int i = 1; i < array.Length; i = i + 2)
+{
+    result += array[i];
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел на нечетных позициях = {result}");
